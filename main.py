76ca395b73cd33e6789555bee799f8b292dbf25d
harrisonospina97 <ospinaharrison11@gmail.com>
2026-03-10 20:46:17 -0500

    Creacion de login o validacion de credenciales

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Harrison Ospina Salazar")  
+print("Harrison Ospina Salazar")
+print("Harrison Ospina Salazar")
+
+user = "harri@uniremington.edu.co"
+password = "123456789"
+
+user_input = input("Ingrese su correo electrónico: ")
+password_input = input("Ingrese su contraseña: ")
+
+if user_input == "harri@uniremington.edu.co" and password_input == "123456789":
+    print("Inicio de sesión exitoso.")
+else:
+    print("Correo electrónico o contraseña incorrectos.")
